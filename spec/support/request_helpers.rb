b7263574ddfb0e63d9b743c4fc66f81ff8f004b0
bstd77 <gbofysil@gmail.com>
2015-10-26 12:48:35 -0400

    Adds authorization for the users controller

--- spec/support/request_helpers.rb
+++ spec/support/request_helpers.rb
@@ -1,11 +1,15 @@
 module Request
+  
   module JsonHelpers
+    
     def json_response
       @json_response ||= JSON.parse(response.body, symbolize_names: true)
     end
+    
   end
 
 	module HeadersHelpers
+    
     def api_header(version = 1)
       request.headers['Accept'] = "application/vnd.marketplace.v#{version}"
     end
@@ -19,5 +23,11 @@ module Request
       api_header
       api_response_format
     end
+
+    def api_authorization_header(token)
+    	request.headers['Authorization'] = token
+    end
+
   end
+
 end
