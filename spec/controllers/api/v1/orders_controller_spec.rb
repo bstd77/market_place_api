1aba503e5edce5f56e0b3ebbae4ff1a560c89505
bstd77 <gbofysil@gmail.com>
2015-10-28 20:52:37 -0400

    Adds the show action for order

--- spec/controllers/api/v1/orders_controller_spec.rb
+++ spec/controllers/api/v1/orders_controller_spec.rb
@@ -1,8 +1,7 @@
 require 'spec_helper'
 
 describe Api::V1::OrdersController do
-
-  describe "GET #index" do
+  describe 'GET #index' do
     before(:each) do
       current_user = FactoryGirl.create :user
       api_authorization_header current_user.auth_token
@@ -10,9 +9,25 @@ describe Api::V1::OrdersController do
       get :index, user_id: current_user.id
     end
 
-    it "returns 4 order records from the user" do
+    it 'returns 4 order records from the user' do
       orders_response = json_response[:orders]
       expect(orders_response).to have(4).items
+    end
+
+    it { should respond_with 200 }
+  end
+
+  describe 'GET #show' do
+    before(:each) do
+      current_user = FactoryGirl.create :user
+      api_authorization_header current_user.auth_token
+      @order = FactoryGirl.create :order, user: current_user
+      get :show, user_id: current_user.id, id: @order.id
+    end
+
+    it 'returns the user order record matching the id' do
+      order_response = json_response[:order]
+      expect(order_response).to eql @order.id
     end
 
     it { should respond_with 200 }
